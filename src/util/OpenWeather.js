228e696a68855e9e4e84ea52d784cb661f08fd7a
Dylan Stringer <riggnroll@Dylan-Stringers-MacBook-Pro.local>
2020-08-31 17:53:34 +0200

    fixed html problem

--- src/util/OpenWeather.js
+++ src/util/OpenWeather.js
@@ -2,7 +2,6 @@ let apiKey = 'ec9d7d715df637631606fb17c5a2b580';
 const fetch = require("node-fetch");
 let units = 'metric';
 let searchMethod;
-let urlStart;
 
 function getSearchMethod(searchTerm) {
   if (searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm) {
@@ -20,8 +19,7 @@ const OpenWeather = {
   searchWeather(searchTerm) {
     getSearchMethod(searchTerm);
     //console.log('searchWeather is running...');
-    location.protocol === 'http:' ? urlStart = 'http' : urlStart = 'https';
-    return fetch(`${urlStart}://api.openweathermap.org/data/2.5/forecast?${searchMethod}=${searchTerm}&APPID=${apiKey}&units=${units}`).then(response => {
+    return fetch(`https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?${searchMethod}=${searchTerm}&APPID=${apiKey}&units=${units}`).then(response => {
       //console.log('result:', result)
       return response.json();
     }).then(jsonResponse => {
