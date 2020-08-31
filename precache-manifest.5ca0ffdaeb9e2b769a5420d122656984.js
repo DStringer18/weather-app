95c21c5934057f88a4121364beca2b12ef8cdff2
Dylan Stringer <riggnroll@Dylan-Stringers-MacBook-Pro.local>
2020-08-31 17:31:40 +0200

    Updates

---
+++ precache-manifest.5ca0ffdaeb9e2b769a5420d122656984.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ace7301d449bac52d7547f81309c9954",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "5901bb8d1dc34603f905",
+    "url": "/weather-app/static/css/main.fc4b0993.chunk.css"
+  },
+  {
+    "revision": "98dbe3030a4cabce9963",
+    "url": "/weather-app/static/js/2.378e2719.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.378e2719.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5901bb8d1dc34603f905",
+    "url": "/weather-app/static/js/main.0b6ba287.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "9836b0bb9a2dc65c77a2eaee55668ca6",
+    "url": "/weather-app/static/media/background.9836b0bb.jpg"
+  },
+  {
+    "revision": "04969f7b6b555c63472c51eea011006e",
+    "url": "/weather-app/static/media/clear.04969f7b.jpg"
+  },
+  {
+    "revision": "c842ece178f35e77e111991c5565eca0",
+    "url": "/weather-app/static/media/cloudy.c842ece1.jpg"
+  },
+  {
+    "revision": "02e3c5e15706265a61b9ecae244650d1",
+    "url": "/weather-app/static/media/rain.02e3c5e1.jpg"
+  },
+  {
+    "revision": "beb6b32ce5f316e775e01cface020b38",
+    "url": "/weather-app/static/media/snow.beb6b32c.jpg"
+  },
+  {
+    "revision": "6868ff7a58c85d85b0bdf06ae8e736fe",
+    "url": "/weather-app/static/media/storm.6868ff7a.jpeg"
+  }
+]);
